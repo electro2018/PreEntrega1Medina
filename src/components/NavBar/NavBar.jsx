ad7588ee47e9461d9593b5d9a69d4b271718ee17
aldo <geroelectronics@hotmail.com>
2022-11-27 05:02:32 -0500

    repro2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,28 @@
 import "./NavBar.css";
-import CartWidget from "../CartWidget"; 
 
  const NavBar = () => {
  return (
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   
     <div className="navbar-container">
        <div className="container-logo"><h2>Electrotienda</h2>
        </div>
        <ul className="navbar">  
        <li className ="navbar-item">
-         <a class="nav-link" href="#">Nosotros</a></li>
+         <a class="nav-link" href="nosotros">Nosotros</a></li>
         <li className ="navbar-item">
-        <a class="nav-link" href="#">Productos</a></li> 
+        <a class="nav-link" href="productos">Productos</a></li> 
         <li className ="navbar-item">
-        <a class="nav-link" href="#">Contactos</a></li>
+        <a class="nav-link" href="contacto">Contactos</a></li>
     </ul>
-    <div className="container-cart"><CartWidget/> </div>
-    </div>
+
+    <div className="container-cart">     
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" color ="red" class="bi bi-cart3" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>   </div>
     
+    </div>
+      </nav> 
  )
 } 
  export default NavBar;
